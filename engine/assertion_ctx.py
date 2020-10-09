c89f723f7a03256b6347de1634f3987b2d91c48f
Sophie Winter <git@phie.me>
2020-10-08 22:54:49 -0700

    Add Matcher.random_matching()

--- engine/assertion_ctx.py
+++ engine/assertion_ctx.py
@@ -1,3 +1,8 @@
+from random import Random
+
 class AssertionCtx:
     def __init__(self):
-        pass
+        self.rand = Random(0) # Just use 0 seed for now
+
+    def random_byte(self):
+        return self.rand.randint(0, 255)
