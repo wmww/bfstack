2f31087d6bb7092c6ad982f96685af9035e90b8f
Sophie Winter <git@phie.me>
2020-11-09 23:01:24 -0800

    Engine: cache loop pairs

--- engine/program.py
+++ engine/program.py
@@ -4,17 +4,18 @@ from errors import ProgramError
 from io_interface  import Io
 from assertion_ctx import AssertionCtx
 
-from typing import List, Optional, Callable
+from typing import List, Optional, Dict
 
 class Program:
     def __init__(self, tape: Tape, code: List[Instruction], io: Io):
         self.tape = tape
-        self.code = code
+        self.code = tuple(code)
         self.current = -1
         self.emulated_ops = 0
         self.real_ops = 0
         self.io = io
         self.assertion_ctx = AssertionCtx()
+        self._cached_loop_pairs: Dict[int, int] = {}
 
     def next_instruction(self) -> Optional[Instruction]:
         if self.current >= len(self.code) - 1:
@@ -35,7 +36,7 @@ class Program:
             raise
         return True
 
-    def find_matching_loop(self, index: int) -> int:
+    def _find_matching_loop(self, index: int) -> int:
         level = self.code[index].loop_level_change()
         assert level != 0, 'find_matching_loop() on non-loop instruction ' + str(self.code[index])
         if level > 0:
@@ -47,3 +48,10 @@ class Program:
             assert index >= 0 and index < len(self.code), 'Unmatched brace (should have been caught in parsing)'
             level += self.code[index].loop_level_change()
         return index
+
+    def matching_loop(self, index: int) -> int:
+        if index not in self._cached_loop_pairs:
+            other = self._find_matching_loop(index)
+            self._cached_loop_pairs[index] = other
+            self._cached_loop_pairs[other] = index
+        return self._cached_loop_pairs[index]
